b22f8584851bed1c6ea2bf98a54990540fb5424c
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2017-09-09 14:32:38 -0400

    lambda event object created as payload and passed. response from lambda parsed, headers, status, and body set

--- lambda_gateway/nginx-conf/conf.d/lambda_invoker.lua
+++ lambda_gateway/nginx-conf/conf.d/lambda_invoker.lua
@@ -1,5 +1,19 @@
 local LambdaService = require "api-gateway.aws.lambda.LambdaService"
 local cjson = require "cjson"
+
+-- convert query request into event payload
+
+local payload = {}
+payload["httpMethod"] = ngx.req.get_method()
+--payload["body"] = ngx.req.get_post_args()
+payload["requestContext"] = {}
+payload["requestContext"]["httpMethod"] = ngx.req.get_method()
+payload["requestContext"]["requestId"] = ngx.var.request_id
+payload["queryStringParameters"] = ngx.req.get_uri_args()
+payload["headers"] = ngx.req.get_headers()
+payload["path"] = ngx.var.uri
+
+
 local service = LambdaService:new({
     aws_region = ngx.var.aws_region,
     aws_credentials = {
@@ -12,17 +26,35 @@ local service = LambdaService:new({
     aws_conn_pool = 100            -- the connection pool size for sockets used to connect to AWS
 })
 
--- convert query params into event payload
--- local payload = cjson.encode({ requestContext = { httpMethod = ngx.req.method } })
-local payload = {}
-payload["requestContext"] = {}
-payload["requestContext"]["httpMethod"] = ngx.req.get_method()
-payload["queryStringParameters"] = ngx.req.get_uri_args()
-payload["headers"] = ngx.req.get_headers()
-
-
 -- invoke the function
 local functionName = ngx.var.aws_function
-local invokeResult, code, headers, status, body  = service:invoke(functionName, payload, context_json)
+local invokeResult, code, headers, status, resp  = service:invoke(functionName, payload, context_json)
 
-ngx.say(tostring(body))
+ngx.log(ngx.WARN,type(resp))
+ngx.log(ngx.WARN,resp)
+
+local resp_tab = cjson.decode(resp)
+ngx.log(ngx.WARN,type(resp_tab))
+
+
+ngx.log(ngx.WARN,type(status))
+ngx.log(ngx.WARN,type(resp_tab["statusCode"]))
+ngx.log(ngx.WARN,resp_tab["statusCode"])
+
+ngx.status = status
+
+-- parse the response like api gate way would
+local body = resp_tab["body"]
+ngx.log(ngx.WARN,type(body))
+
+ngx.log(ngx.WARN,type(resp_tab["headers"]))
+for header, value in pairs(resp_tab["headers"]) do 
+    ngx.log(ngx.WARN,type(header))
+    ngx.log(ngx.WARN,header)
+    ngx.log(ngx.WARN,type(value))
+    ngx.log(ngx.WARN,value)
+     
+    ngx.header[header] = value
+end
+
+ngx.say(body)
